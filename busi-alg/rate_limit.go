f408e228f0250f47ed1ddd895b93ff9408acefb2
jrmarcco <jrmarcco@gmail.com>
2022-11-21 20:50:52 +0800

    msg

--- busi-alg/rate_limit.go
+++ busi-alg/rate_limit.go
@@ -5,8 +5,8 @@ import (
 	"time"
 )
 
-// RateLimiter 令牌桶
-type RateLimiter struct {
+// TokenBucket 令牌桶
+type TokenBucket struct {
 	rate   int64 // 令牌放入速度
 	max    int64 // 令牌最大数量
 	last   int64 // 上一次请求发生时间
@@ -18,9 +18,9 @@ func cur() int64 {
 	return time.Now().Unix()
 }
 
-func New(rate, max int64) *RateLimiter {
+func New(rate, max int64) *TokenBucket {
 	// TODO check max & rate
-	return &RateLimiter{
+	return &TokenBucket{
 		rate:   rate,
 		max:    max,
 		amount: max,
@@ -28,17 +28,17 @@ func New(rate, max int64) *RateLimiter {
 	}
 }
 
-func (r *RateLimiter) Pass() bool {
-	r.m.Lock()
-	defer r.m.Unlock()
+func (t *TokenBucket) Pass() bool {
+	t.m.Lock()
+	defer t.m.Unlock()
 
 	// 时间间隔
-	interval := cur() - r.last
+	interval := cur() - t.last
 
 	// 超过最大数量则不继续补充令牌
-	amount := r.amount + interval*r.rate
-	if amount > r.max {
-		amount = r.max
+	amount := t.amount + interval*t.rate
+	if amount > t.max {
+		amount = t.max
 	}
 
 	// 没有令牌则请求不通过
@@ -48,8 +48,8 @@ func (r *RateLimiter) Pass() bool {
 
 	// 取出令牌
 	amount--
-	r.amount = amount
-	r.last = cur()
+	t.amount = amount
+	t.last = cur()
 
 	return true
 }
