cdda2b42b9773c73e85e7e4f9f7e98cd582c7552
jrmarcco <jrmarcco@gmail.com>
2025-03-19 14:01:15 +0800

    chore: reinit

--- grpc/pcbook/db/generate.go
+++
@@ -1,4 +0,0 @@
-package db
-
-// go run -mod=mod entgo.io/ent/cmd/ent init Laptop --target ./grpc/pcbook/db/ent/schema
-//go:generate go run -mod=mod entgo.io/ent/cmd/ent generate ./schema --idtype "uint64"
--- ref/copy/copier.go
+++
@@ -1,6 +0,0 @@
-package copy
-
-type Copier[S, D any] interface {
-	Copy(src *S) (*D, error)
-	CopyTo(src *S, dst *D) error
-}
